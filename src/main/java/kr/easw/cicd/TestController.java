3507c30bd08048c329b741e08c7c1a6c4ad57c24
22DT <qab3312@gmail.com>
2025-01-04 21:02:39 +0900

    add TestController

---
+++ src/main/java/kr/easw/cicd/TestController.java
@@ -0,0 +1,21 @@
+package kr.easw.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test1")
+    public ResponseEntity<Void> test1() {
+        System.out.println("test1");
+        return ResponseEntity.noContent().build();
+    }
+
+    //@GetMapping("/test2")
+    public ResponseEntity<Void> test2() {
+        System.out.println("test2");
+        return ResponseEntity.noContent().build();
+    }
+}
